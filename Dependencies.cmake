b20dce52882b0f5af8df2e3b3f9f98fdd0253437
Niclas Larsson <niclas.larsson.3@volvocars.com>
2025-12-12 00:00:21 +0100

    Remove cli11 since it's not used

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -12,10 +12,6 @@ function(ev_loop_setup_dependencies)
     cpmaddpackage("gh:catchorg/Catch2@3.8.1")
   endif()
 
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.5.0")
-  endif()
-
   if(NOT TARGET tools::tools)
     cpmaddpackage("gh:lefticus/tools#update_build_system")
   endif()
