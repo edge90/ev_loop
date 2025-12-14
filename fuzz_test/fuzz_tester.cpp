9bdbd7f46368008d53e7a1453f5dfad41e9d4e3d
Niclas Larsson <niclas.larsson.3@volvocars.com>
2025-12-14 20:44:08 +0100

    clang-format

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -3,7 +3,4 @@
 
 // TODO: Implement fuzz testing for ev_loop
 // cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t * /*Data*/, size_t /*Size*/)
-{
-  return 0;
-}
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t * /*Data*/, size_t /*Size*/) { return 0; }
